449835c6d13ce1d8c6cd180bd8c1dbc71b0d8298
csuarezgurruchaga <csuarezgurruchaga@gmail.com>
2022-02-09 16:55:32 -0300

    Trabajo 4 casi terminado

---
+++ Trabajo Practico/Practico_4/modulo_transformadores_custom_g5.py
@@ -0,0 +1,48 @@
+
+class MakeDummies(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        X1 = pd.get_dummies(X, columns = self.columns, drop_first = False)
+        self.feature_names = X1.columns.tolist()
+        return X1
+    
+    def get_feature_names(self):
+        return self.feature_names
+            
+    
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, features):
+        self.features = features
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        X1 = pd.DataFrame(X[self.features])
+        self.feature_names = X1.columns.tolist()
+        return pd.DataFrame(X[self.features])
+    
+    def get_feature_names(self):
+        return self.feature_names
+      
+    
+class MakeInt(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        X1 = pd.DataFrame(X[self.columns].astype(int))
+        self.feature_names = X1.columns.tolist()
+        return pd.DataFrame(X[self.columns].astype(int))
+    
+    def get_feature_names(self):
+        return self.feature_names
+    
